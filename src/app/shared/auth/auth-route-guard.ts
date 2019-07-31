cc7e0aabbcb25befd33b9c0ccd3a765502ce5126
Sing-T <nemo.singt@gmail.com>
2019-08-01 00:09:26 +0800

    nothing matters

--- src/app/shared/auth/auth-route-guard.ts
+++ src/app/shared/auth/auth-route-guard.ts
@@ -18,11 +18,11 @@ export class AppRouteGuard implements CanActivate, CanActivateChild {
       return false;
     }
 
-    if (!route.data || !route.data['permission']) {
+    if (!route.data || !route.data.permission) {
       return true;
     }
 
-    if (this._permissionChecker.isGranted(route.data['permission'])) {
+    if (this._permissionChecker.isGranted(route.data.permission)) {
       return true;
     }
 
